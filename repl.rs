1cd26fd6c5723ffd965f9bfb6724651e77d340f8
nick.iammarino <nickiammarino@gmail.com>
2024-03-02 19:20:41 -0500

    set up memory only table with insert and select commands

--- repl.rs
+++ repl.rs
@@ -1,18 +1,78 @@
 use std::{
+    boxed::Box,
     io::{self, Write},
     process,
 };
 
-enum StatementType {
-    Insert,
-    Select,
-}
+const PAGE_SIZE: usize = 10;
 
+#[derive(Debug)]
 enum ReplError {
-    ArgumentError,
+    BadRowNumber,
+    RowNumberNotFound,
+    BadKey,
+    BadUsername,
+    BadEmail,
+    TableIndexError,
+}
+use crate::ReplError::*;
+#[derive(Debug)]
+enum ReplMessage {
+    Executed,
+    Fetched(Row),
+}
+use crate::ReplMessage::*;
+
+type InsertArgs<'a> = Box<dyn Iterator<Item = &'a str> + 'a>;
+enum StatementType<'a> {
+    Insert { args: InsertArgs<'a> },
+    Select { args: InsertArgs<'a> },
+}
+use crate::StatementType::*;
+
+// -- Table stuff for in memory impl
+#[derive(Debug, Clone)]
+struct Row {
+    id: usize,
+    username: String,
+    email: String, // could make special type for this
+}
+#[derive(Debug)]
+struct Page {
+    // need to serialize eventually
+    max_size: usize,
+    rows: Vec<Row>,
+}
+#[derive(Debug)]
+struct Table {
+    num_rows: usize,
+    pages: Vec<Page>, // need to serialize eventually
 }
 
-use crate::StatementType::*;
+impl Table {
+    fn push_row(&mut self, row: Row) {
+        if self.num_rows % PAGE_SIZE == 0 {
+            self.pages.push(Page {
+                max_size: PAGE_SIZE,
+                rows: vec![],
+            });
+        }
+
+        let page_index = self.num_rows / PAGE_SIZE;
+        self.pages[page_index].rows.push(row);
+        self.num_rows += 1;
+    }
+    fn get_row(&self, row_number: usize) -> Result<Row, ReplError> {
+        if row_number + 1 > self.num_rows {
+            return Err(TableIndexError);
+        }
+
+        let (page_index, row_index) = (row_number / PAGE_SIZE, row_number % PAGE_SIZE);
+        Ok(self.pages[page_index].rows[row_index].clone())
+    }
+}
+
+// ------------
 
 fn print_prompt() {
     print!("db > ");
@@ -20,41 +80,42 @@ fn print_prompt() {
 }
 
 fn main() {
-    let mut input_buffer = String::new();
+    let mut table = Table {
+        num_rows: 0,
+        pages: vec![],
+    };
     println!("WELCOME TO SHITTY SQL REPL");
     loop {
+        let mut input_buffer = String::new();
         print_prompt();
         io::stdin()
             .read_line(&mut input_buffer)
             .expect("Failed to read line");
 
         if input_buffer.chars().next() == Some('.') {
-            match do_meta_command(&input_buffer) {
+            match do_meta_command(&input_buffer, &table) {
                 Ok(_) => {}
                 Err(_) => {
                     println!("Unrecognized command {}", input_buffer);
                 }
             }
-            input_buffer.clear();
         } else {
             match prepare_statement(&input_buffer) {
                 Ok(statement) => {
-                    execute_statement(statement);
-                    println!("Executed.");
-                    break;
+                    execute_statement(statement, &mut table);
                 }
                 Err(_) => {
                     println!("Unrecognized keyword at start of {}", input_buffer);
-                    input_buffer.clear()
                 }
             }
         }
     }
 }
 
-fn do_meta_command(command: &String) -> Result<(), ()> {
+fn do_meta_command(command: &String, table: &Table) -> Result<(), ()> {
     match command.as_str().trim() {
         ".exit" => {
+            println!("{table:?}");
             process::exit(0);
         }
         _ => Err(()),
@@ -65,15 +126,81 @@ fn prepare_statement(input_buffer: &String) -> Result<StatementType, ()> {
     let mut statement = input_buffer.as_str().split_whitespace();
     let stype = statement.next().unwrap();
     match stype {
-        "insert" => Ok(Insert),
-        "select" => Ok(Select),
+        "insert" => Ok(Insert {
+            args: Box::new(statement),
+        }),
+        "select" => Ok(Select {
+            args: Box::new(statement),
+        }),
         _ => Err(()),
     }
 }
 
-fn execute_statement(statement: StatementType) {
-    match statement {
-        Insert => println!("Do insert here..."),
-        Select => println!("Do select here..."),
+fn execute_statement(statement: StatementType, table: &mut Table) {
+    let res: Result<_, ReplError> = match statement {
+        Insert { args } => execute_insert(args, table),
+        Select { args } => execute_select(args, table),
+    };
+
+    let message = match res {
+        Ok(msg) => format!("{msg:?}"),
+        Err(e) => format!("Error: {e:?}"),
+    };
+
+    println!("{message}")
+}
+
+fn execute_select(
+    mut statement_args: InsertArgs,
+    table: &mut Table,
+) -> Result<ReplMessage, ReplError> {
+    match statement_args.next() {
+        Some(s) => match s.parse::<usize>() {
+            Ok(n) => match table.get_row(n - 1) {
+                Ok(row) => Ok(Fetched(row)),
+                Err(e) => Err(e),
+            },
+            _ => Err(BadRowNumber),
+        },
+        _ => Err(RowNumberNotFound),
     }
+}
+
+fn execute_insert(
+    mut statement_args: InsertArgs,
+    table: &mut Table,
+) -> Result<ReplMessage, ReplError> {
+    // probably do zip with defn to generalize
+    let id: usize = match statement_args.next() {
+        Some(s) => match s.parse() {
+            Ok(n) => n,
+            _ => return Err(BadKey),
+        },
+        _ => {
+            return Err(BadKey);
+        }
+    };
+
+    let username = match statement_args.next() {
+        Some(s) => s.to_string(),
+        _ => {
+            return Err(BadUsername);
+        }
+    };
+
+    let email = match statement_args.next() {
+        Some(s) => s.to_string(),
+        _ => {
+            return Err(BadEmail);
+        }
+    };
+
+    let row = Row {
+        id,
+        username,
+        email,
+    };
+
+    table.push_row(row);
+    Ok(Executed)
 }
