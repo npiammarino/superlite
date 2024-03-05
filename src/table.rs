704945164e5c8e57841f1c6e1306ea2416cb0b3c
nick.iammarino <nickiammarino@gmail.com>
2024-03-04 23:27:30 -0500

    updated table to store bytes and prepare for writing bytes to file

--- src/table.rs
+++ src/table.rs
@@ -1,68 +1,158 @@
+use std::{fs::OpenOptions, mem, str};
+
 #[derive(Debug)]
 pub enum TableError {
     TableIndexError,
+    TableFull,
 }
 use crate::TableError::*;
 
-const PAGE_SIZE: usize = 10;
+const USERNAME_MAX: usize = 32;
+const EMAIL_MAX: usize = 255;
 
+type ColumnId = u32;
+type ColumnUsername = [u8; USERNAME_MAX];
+type ColumnEmail = [u8; EMAIL_MAX];
+
+const ID_SIZE: usize = mem::size_of::<ColumnId>();
+const USERNAME_SIZE: usize = mem::size_of::<ColumnUsername>();
+const EMAIL_SIZE: usize = mem::size_of::<ColumnEmail>();
+const ID_OFFSET: usize = 0;
+const USERNAME_OFFSET: usize = ID_OFFSET + ID_SIZE;
+const EMAIL_OFFSET: usize = USERNAME_OFFSET + USERNAME_SIZE;
+const ROW_SIZE: usize = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const PAGE_SIZE: usize = 4096;
+const TABLE_MAX_PAGES: usize = 100;
+const ROWS_PER_PAGE: usize = PAGE_SIZE / ROW_SIZE;
+const TABLE_MAX_ROWS: usize = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+#[derive(Debug, Clone, Copy)]
+pub struct RowBytes {
+    id: ColumnId,
+    username: ColumnUsername,
+    email: ColumnEmail,
+}
 #[derive(Debug, Clone)]
 pub struct Row {
-    id: usize,
+    id: u32,
     username: String,
-    email: String, // could make special type for this
+    email: String,
 }
 
 impl Row {
-    pub fn build(id: usize, username: String, email: String) -> Row {
+    pub fn build(id: u32, username: String, email: String) -> Row {
         Row {
             id,
             username,
             email,
         }
     }
+
+    fn serialize(self) -> RowBytes {
+        let mut username = [b'\0'; USERNAME_MAX];
+        let chars = self.username.as_bytes();
+        for i in 0..chars.len() {
+            username[i] = chars[i];
+        }
+
+        let mut email = [b'\0'; EMAIL_MAX];
+        let chars = self.username.as_bytes();
+        for i in 0..chars.len() {
+            email[i] = chars[i];
+        }
+
+        RowBytes {
+            id: self.id,
+            username,
+            email,
+        }
+    }
 }
 
-#[derive(Debug)]
-pub struct Page {
-    // need to serialize eventually
-    max_size: usize,
-    rows: Vec<Row>,
+impl RowBytes {
+    fn deserialize(self) -> Row {
+        Row {
+            id: self.id,
+            username: String::from(str::from_utf8(&self.username).expect("mem")),
+            email: String::from(str::from_utf8(&self.email).expect("mem")),
+        }
+    }
 }
+
+#[derive(Debug, Clone, Copy)]
+pub struct Page {
+    rows: [Option<RowBytes>; ROWS_PER_PAGE],
+}
+
+impl Page {
+    fn new() -> Page {
+        Page {
+            rows: [None; ROWS_PER_PAGE],
+        }
+    }
+}
+
+// struct Pager {
+//     file: OpenOptions,
+//     pages: [Option<Page>; TABLE_MAX_PAGES],
+// }
+
+// impl Pager {
+//     fn open(filename: String) -> Result<Pager, ()> {
+//         let f = OpenOptions::new().read(true).write(true).open(filename)?;
+//         let pages = [None; TABLE_MAX_PAGES];
+
+//         Pager { file: f, pages }
+//     }
+// }
 
 #[derive(Debug)]
 pub struct Table {
     num_rows: usize,
-    pages: Vec<Page>, // need to serialize eventually
+    pages: [Option<Page>; TABLE_MAX_PAGES],
+    // pager: Pager,
 }
 
 impl Table {
-    pub fn new() -> Table {
-        Table {
-            num_rows: 0,
-            pages: Vec::new(),
-        }
-    }
-
-    pub fn push_row(&mut self, row: Row) {
-        if self.num_rows % PAGE_SIZE == 0 {
-            self.pages.push(Page {
-                max_size: PAGE_SIZE,
-                rows: vec![],
-            });
+    fn row_slot(&mut self, row_num: usize) -> (usize, usize) {
+        let page_index = row_num / ROWS_PER_PAGE;
+        let row_index = row_num % ROWS_PER_PAGE;
+        if self.pages[page_index].is_none() {
+            self.pages[page_index].replace(Page::new());
         }
 
-        let page_index = self.num_rows / PAGE_SIZE;
-        self.pages[page_index].rows.push(row);
-        self.num_rows += 1;
+        (page_index, row_index)
+    }
+    // pub fn open() -> Table {
+    //     let filename = String::new();
+    //     let pager = Pager::open(filename);
+
+    //     Table { num_rows: 0, pager }
+    // }
+
+    pub fn push_row(&mut self, row: Row) -> Result<(), TableError> {
+        if self.num_rows == TABLE_MAX_ROWS {
+            return Err(TableFull);
+        }
+
+        let (page_index, row_index) = self.row_slot(self.num_rows + 1);
+        let mut page = self.pages[page_index].expect("Created in slot function");
+        page.rows[row_index].replace(row.serialize());
+
+        // let page_index = self.num_rows / PAGE_SIZE;
+        // self.pages[page_index].rows.push(row);
+        // self.num_rows += 1;
+        Ok(())
     }
 
-    pub fn get_row(&self, row_number: usize) -> Result<Row, TableError> {
-        if row_number + 1 > self.num_rows {
+    pub fn get_row(&mut self, row_number: usize) -> Result<Row, TableError> {
+        if row_number > self.num_rows {
             return Err(TableIndexError);
         }
 
-        let (page_index, row_index) = (row_number / PAGE_SIZE, row_number % PAGE_SIZE);
-        Ok(self.pages[page_index].rows[row_index].clone())
+        let (page_index, row_index) = self.row_slot(row_number);
+        let row_bytes = self.pages[page_index].expect("temp").rows[row_index].expect("temp");
+        Ok(row_bytes.deserialize())
     }
 }
